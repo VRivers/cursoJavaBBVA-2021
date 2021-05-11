c2c8bb485b2c05fa88709a0255f4f643fb50a43b
VRivers <vicriver94@gmail.com>
2021-05-11 09:54:33 +0200

    Lectura y escritura de ficheros

---
+++ src/archivos/EscribeFichero.java
@@ -0,0 +1,34 @@
+package archivos;
+
+import java.io.FileWriter;
+import java.io.PrintWriter;
+
+public class EscribeFichero
+{
+    public static void main(String[] args)
+    {
+        FileWriter fichero = null;
+        PrintWriter pw = null;
+        try
+        {
+            fichero = new FileWriter("C:\\Users\\Victor Rivera\\javaworkspace\\cursoJava\\archivos\\ficheroEscritura.txt");
+            pw = new PrintWriter(fichero);
+
+            for (int i = 0; i < 10; i++)
+                pw.println("Casas " + i);
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+           try {
+           // Nuevamente aprovechamos el finally para 
+           // asegurarnos que se cierra el fichero.
+           if (null != fichero)
+              fichero.close();
+           } catch (Exception e2) {
+              e2.printStackTrace();
+           }
+        }
+        System.out.println("finalizo felizmente");
+    }
+}
