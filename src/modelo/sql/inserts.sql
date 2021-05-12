e2c892d8d1c222baf1f7fc33b38729a94d506e61
VRivers <vicriver94@gmail.com>
2021-05-12 13:23:08 +0200

    delete y select SQL

--- src/modelo/sql/inserts.sql
+++ src/modelo/sql/inserts.sql
@@ -8,23 +8,3 @@ insert into figuras(fig_tipo, fig_nombre, fig_base, fig_altura)
             values (3, "rect1", 10, 8.4)
 insert into figuras(fig_tipo, fig_nombre, fig_lado, fig_numlados, fig_apotema)
             values (5, "polig1", 10, 6, 6.2)
-            
-update figuras
-set fig_nombre = "cuadrado"
-where fig_tipo = 1
-
-update figuras
-set fig_nombre = "circulo"
-where fig_tipo = 2
-
-update figuras
-set fig_nombre = "rectamgulo"
-where fig_tipo = 3
-
-update figuras
-set fig_nombre = "triangulo"
-where fig_tipo = 4
-
-update figuras
-set fig_nombre = "poligono"
-where fig_tipo = 5
---
+++ src/modelo/sql/updates.sql
@@ -0,0 +1,20 @@
+
+update figuras
+set fig_nombre = "cuadrado"
+where fig_tipo = 1
+
+update figuras
+set fig_nombre = "circulo"
+where fig_tipo = 2
+
+update figuras
+set fig_nombre = "rectamgulo"
+where fig_tipo = 3
+
+update figuras
+set fig_nombre = "triangulo"
+where fig_tipo = 4
+
+update figuras
+set fig_nombre = "poligono"
+where fig_tipo = 5
