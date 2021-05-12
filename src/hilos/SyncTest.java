9f2855f53a7239f2258f06669108d00ad6131ff2
VRivers <vicriver94@gmail.com>
2021-05-12 13:05:46 +0200

    consultas SQL

--- src/hilos/SyncTest.java
+++ src/hilos/SyncTest.java
@@ -8,18 +8,22 @@ public class SyncTest {
 
     Producer p1 = new Producer(stack);
     Thread prodT1 = new Thread (p1);
+    prodT1.setPriority(Thread.MAX_PRIORITY);
     prodT1.start();
 
     Producer p2 = new Producer(stack);
     Thread prodT2 = new Thread (p2);
+    prodT2.setPriority(Thread.MIN_PRIORITY);
     prodT2.start();
 
     Consumer c1 = new Consumer(stack);
     Thread consT1 = new Thread (c1);
+    prodT1.setPriority(Thread.NORM_PRIORITY);
     consT1.start();
 
     Consumer c2 = new Consumer(stack);
     Thread consT2 = new Thread (c2);
+    prodT2.setPriority(Thread.NORM_PRIORITY);
     consT2.start();
   }
 }
